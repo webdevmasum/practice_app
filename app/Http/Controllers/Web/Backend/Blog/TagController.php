9af2d60fdfe9175607f2e30560af5aa04103ffaf
webdevmasum <masumislam0018@gmail.com>
2025-07-15 23:39:54 +0600

    best practice module controller

---
+++ app/Http/Controllers/Web/Backend/Blog/TagController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Web\Backend\Blog;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class TagController extends Controller
+{
+    //
+}
